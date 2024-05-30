7d213888865abd1157eb55ba3ee5e47879632d9c
punchlemon <tsukiwahigashini0410@gmail.com>
2024-05-30 14:08:22 +0900

    I edited the file to bypass the norminette. Also, I slightly modified the arguments passed to the test function.

--- src/test_ft_printf_mix.c
+++ src/test_ft_printf_mix.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test_ft_printf_mix.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: retanaka <retanaka@student.42.tokyo>       +#+  +:+       +#+        */
+/*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 09:10:31 by retanaka          #+#    #+#             */
-/*   Updated: 2024/05/22 17:59:23 by retanaka         ###   ########.fr       */
+/*   Updated: 2024/05/30 12:18:06 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,14 @@
 void	test_ft_printf_mix(void)
 {
 	printf("\n-------- mix test--------\n\n");
-	printf(" | return = %d\n", ft_printf("ft_printf : [%c%s%p%d%d%i%u%x%X]", 42, "42", NULL, 42, 42, 42, 42, 42, 42));
-	printf(" | return = %d\n\n", printf("   printf : [%c%s%p%d%d%i%u%x%X]", 42, "42", NULL, 42, 42, 42, 42, 42, 42));
-	printf(" | return = %d\n", ft_printf("ft_printf : [%d%d%d]", INT_MAX, INT_MIN, 0));
-	printf(" | return = %d\n\n", printf("   printf : [%d%d%d]", INT_MAX, INT_MIN, 0));
+	printf(" | return = %d\n",
+		ft_printf("ft_printf : [%c%s%p%d%i%u%x%X%%]",
+			42, "42", NULL, 0, 1, 2, 3, 4));
+	printf(" | return = %d\n\n",
+		printf("   printf : [%c%s%p%d%i%u%x%X%%]",
+			42, "42", NULL, 0, 1, 2, 3, 4));
+	printf(" | return = %d\n",
+		ft_printf("ft_printf : [%d%d%d]", INT_MAX, INT_MIN, 0));
+	printf(" | return = %d\n\n",
+		printf("   printf : [%d%d%d]", INT_MAX, INT_MIN, 0));
 }
